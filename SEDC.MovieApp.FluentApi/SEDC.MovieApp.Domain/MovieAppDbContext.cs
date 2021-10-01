de61b1a92ac0cfd81aa8b442dc0bbc5bcb8fcee1
VaskoPav <79167640+VaskoPav@users.noreply.github.com>
2021-10-01 21:22:44 +0200

    First Commit

---
+++ SEDC.MovieApp.FluentApi/SEDC.MovieApp.Domain/MovieAppDbContext.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEDC.MovieApp.Domain
+{
+    public class MovieAppDbContext:DbContext
+    {
+        public MovieAppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Movie>()
+               .Property(x => x.Id)
+               .HasMaxLength(30)
+               .IsRequired();
+
+            modelBuilder.Entity<Movie>()
+                .Property(x => x.Title)
+                .HasMaxLength(30);
+                
+            modelBuilder.Entity<Movie>()
+                .Property(x => x.Description)
+                .HasMaxLength(30);
+            modelBuilder.Entity<Movie>()
+                .Property(x => x.Year)
+                .HasMaxLength(30);
+
+
+            modelBuilder.Entity<Movie>()
+               .Property(x => x.Genre)
+               .HasMaxLength(30);
+               
+        }
+    }
+}
---
+++ SEDC.MovieApp.FluentApi/SEDC.MovieApp.Helpers/AppSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEDC.MovieApp.Helpers
+{
+    public class AppSettings
+    {
+        public string ConnectionString { get; set; }
+    }
+}
